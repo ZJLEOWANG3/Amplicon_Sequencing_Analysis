833ec80d7c271f0cd00a24ff7658934a7bc115f2
Guangyu Li <li.gua@husky.neu.edu>
2019-10-28 18:53:41 -0400

    added database download script

--- custom_scripts/link_ref_db.sh
+++ custom_scripts/link_ref_db.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
+################################################################################
 # this script links the reference database used by mothur.script when run jobs
 # on dicsovery@NEU
 ################################################################################
+db_dir="/home/li.gua/scratch/DATABASE"
+ln_dir="./ref_db"
+mkdir -p $ln_dir
+################################################################################
+# silva v123
+ln -sfT $db_dir/MOTHUR_DB/silva_v123/silva.seed_v123.align.v4	$ln_dir/silva.seed_v123.align.v4
+ln -sfT $db_dir/MOTHUR_DB/silva_v123/silva.nr_v123.align.v4		$ln_dir/silva.nr_v123.align.v4
+ln -sfT $db_dir/MOTHUR_DB/silva_v123/silva.nr_v123.tax			$ln_dir/silva.nr_v123.tax
+# silva_v128
+ln -sfT $db_dir/MOTHUR_DB/silva_v128/silva.seed_v128.align.v4	$ln_dir/silva.seed_v128.align.v4
+ln -sfT $db_dir/MOTHUR_DB/silva_v128/silva.nr_v128.align.v4		$ln_dir/silva.nr_v128.align.v4
+ln -sfT $db_dir/MOTHUR_DB/silva_v128/silva.nr_v128.tax			$ln_dir/silva.nr_v128.tax
+# silva_v132
+ln -sfT $db_dir/MOTHUR_DB/silva_v132/silva.seed_v132.align.v4	$ln_dir/silva.seed_v132.align.v4
+ln -sfT $db_dir/MOTHUR_DB/silva_v132/silva.nr_v132.align.v4		$ln_dir/silva.nr_v132.align.v4
+ln -sfT $db_dir/MOTHUR_DB/silva_v132/silva.nr_v132.tax			$ln_dir/silva.nr_v132.tax
+# midas, built on silva v123, should be better aligned with silva v123
+ln -sfT $db_dir/MIDAS_DB/MiDAS_S123_2.1.3.fasta					$ln_dir/midas_s123.fasta
+ln -sfT $db_dir/MIDAS_DB/MiDAS_S123_2.1.3.mothur.tax			$ln_dir/midas_s123.mothur.tax
+################################################################################
 
-mkdir -p ref_db
-ln -sfT /home/li.gua/scratch/DATABASE/MIDAS_DB/MiDAS_S123_2.1.3.fasta \
-	./ref_db/MiDAS_S123_2.1.3.fasta
-ln -sfT /home/li.gua/scratch/DATABASE/MIDAS_DB/MiDAS_S123_2.1.3.mothur.tax \
-	./ref_db/MiDAS_S123_2.1.3.mothur.tax
-ln -sfT /home/li.gua/scratch/DATABASE/MOTHUR_DB/silva_v123/silva.nr_v123.v4.align \
-	./ref_db/silva.nr_v123.v4.align
+################################################################################
+# only need to change these
+################################################################################
+# alignment template, used in template (a.k.a. reference) in align.seqs()
+# use seed in alignment is fine
+ln -sfT $ln_dir/silva.seed_v128.align.v4						$ln_dir/align.fasta
+# taxonomy classif. reference and labels
+# NOT use seed seqs here
+ln -sfT $ln_dir/silva.nr_v128.align.v4							$ln_dir/classif.ref
+ln -sfT $ln_dir/silva.nr_v128.tax								$ln_dir/classif.tax
+# or, use midas as reference
+#ln -sfT $ln_dir/midas_s123.fasta								$ln_dir/classif.ref
+#ln -sfT $ln_dir/midas_s123.tax									$ln_dir/classif.tax
