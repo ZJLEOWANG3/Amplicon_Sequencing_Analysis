e86ede14619d35d196d66fcd6ba2bf9c3ae1dd88
Guangyu Li <li.gua@husky.neu.edu>
2020-01-06 15:08:04 -0500

    added count table to lite pack

--- script/pack_output_lite.sh
+++ script/pack_output_lite.sh
@@ -4,7 +4,8 @@
 tar -jcf mothur.output.lite.tar.bz2 \
 	mothur.*.logfile \
 	sbatch.mothur.* \
-	mothur.output/*.pick.pick.fasta \
+	mothur.output/*.precluster.pick.pick.fasta \
+	mothur.output/*.denovo.vsearch.pick.pick.count_table \
 	mothur.output/*.opti_mcc.* \
 	mothur.output.shared \
 	mothur.output.tax
