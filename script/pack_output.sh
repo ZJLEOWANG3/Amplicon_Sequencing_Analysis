0bfee0b1a2dc5c565d077397d0f6c65a7f01871e
Guangyu Li <li.gua@husky.neu.edu>
2020-08-18 15:18:41 -0400

    minor update to the mothur script and output packing on discovery cluster

--- script/pack_output.sh
+++ script/pack_output.sh
@@ -2,7 +2,9 @@
 
 # pack all mothur outputs
 tar -jcf mothur.output.tar.bz2 \
+	--exclude "*.tar.bz2" \
 	mothur.*.logfile \
 	sbatch.mothur.* \
+	mothur.input.list \
 	mothur.output/ \
 	mothur.output.*
