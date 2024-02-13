281e107854090d53b59347eee9014e646237a365
Guangyu Li <gl343@cornell.edu>
2024-02-13 17:34:46 -0500

    exclude dist in lite pack

--- script/pack_output.lite.sh
+++ script/pack_output.lite.sh
@@ -3,6 +3,7 @@
 # pack only outputs necessary for downstream analysis
 tar -jcf mothur.output.lite.tar.bz2 \
 	--exclude "*.tar.bz2" \
+	--exclude "*.dist" \
 	mothur.*.logfile \
 	mothur.input.list \
 	mothur.output/final.* \
