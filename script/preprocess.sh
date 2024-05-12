bbde916e316c4a99804cfd0ef5ba16b8478510be
LeoWang311 <88264474+ZJLEOWANG3@users.noreply.github.com>
2024-05-12 11:03:11 -0400

    Update preprocess.sh

--- script/preprocess.sh
+++ script/preprocess.sh
@@ -5,7 +5,7 @@ qc_dir="./reads_qc"
 
 mkdir -p $qc_dir
 for i in $(cat samples.list); do
-	sickle pe -t sanger \
+	/home/a.onnis-hayden/opt/sickle/1.33/bin/sickle pe -t sanger \
 		-f $rd_dir/$i"_R1.fastq" \
 		-r $rd_dir/$i"_R2.fastq" \
 		-o $qc_dir/$i"_R1.fastq" \
